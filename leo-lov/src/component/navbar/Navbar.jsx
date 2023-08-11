ebab18a7a57abdc2129da84ffdfcbd86fe1a6ad7
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-08-11 11:05:00 +0200

    fonts are in

--- leo-lov/src/component/navbar/Navbar.jsx
+++ leo-lov/src/component/navbar/Navbar.jsx
@@ -1,17 +1,25 @@
-import style from './navbar.module.scss'
+import style from "./navbar.module.scss";
 
-export function Navbar(){
-    return (
-        <section>
-            <nav>
-                <ul>
-                    <li><a href="#">Hjem</a></li>
-                    <li><a href="#">Advokaterne</a></li>
-                    <li><a href="#">Om LeoLov</a></li>
-                    <li><a href="#">Kontakt</a></li>
-                </ul>
-            </nav>
-            <img src="#" alt="LeoLov logo" />
-        </section>
-    )
+export function Navbar() {
+  return (
+    <section>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Hjem</a>
+          </li>
+          <li>
+            <a href="#">Advokaterne</a>
+          </li>
+          <li>
+            <a href="#">Om LeoLov</a>
+          </li>
+          <li>
+            <a href="#">Kontakt</a>
+          </li>
+        </ul>
+      </nav>
+      <img src="#" alt="LeoLov logo" />
+    </section>
+  );
 }
